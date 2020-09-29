6f0d2044c8c43066433191e7e5d038091906c5f4
clarkchen84 <sizhe.chen1@gmail.com>
2020-09-29 20:32:07 +0800

    commit

--- coffee-bucks/src/main/java/sizhe/chen/coffeebucks/bean/Coffee.java
+++ coffee-bucks/src/main/java/sizhe/chen/coffeebucks/bean/Coffee.java
@@ -1,2 +1,37 @@
-package sizhe.chen.coffeebucks.bean;public class Coffee {
+package sizhe.chen.coffeebucks.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.joda.money.Money;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name="T_MENU")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coffee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    @Column
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",@org.hibernate.annotations.Parameter(name = "currencyCode",value="CNY"))
+    private Money price;
+
+    @CreationTimestamp
+    private Date createDateTime;
+    @UpdateTimestamp
+    private Date updateDateTime;
 }
